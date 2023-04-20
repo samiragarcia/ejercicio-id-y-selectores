32a6703dcddfa1ae029e5359c0808c6d43be6d3a
samira garcia <samira.garcia95@gmail.com>
2023-04-20 12:36:12 -0300

    terminado

---
+++ script.js
@@ -0,0 +1,12 @@
+const imagenGatitoCute = document.querySelector("#imagen-gatito-cute");
+const imagenGatito = document.querySelector("#imagen-gatito");
+const cuerpoPagina = document.querySelector("body");
+const tituloGatito = document.querySelector("#imagen-gatito");
+const linkPagina = document.querySelector("#link-pagina");
+
+tituloGatito.style.color = "red";
+cuerpoPagina.style.color = "pink";
+tituloGatito.innerHTML = "imagen gatito";
+linkPagina.innerHTML = "link pagina";
+linkPagina.href = "https://www.mercadolibre.com.ar/#from=homecom";
+imagenGatito.src = "https://previews.123rf.com/images/totostark/totostark2106/totostark210600006/170584576-lindo-gato-naranja-patas-arriba-sobre-dibujos-animados-de-pared-ilustraci%C3%B3n-vectorial.jpg";
